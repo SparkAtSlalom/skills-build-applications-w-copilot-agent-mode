bc659fa3b0c33b1fc6c75e657b5f45d3d33c712f
Sung Park <sung.park@slalom.com>
2025-08-07 17:41:11 +0000

    Reconfigured project to use mongoengine and fixed view imports in urls.py

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import UserListView, UserDetailView, TeamListView, TeamDetailView, ActivityListView, ActivityDetailView, LeaderboardListView, LeaderboardDetailView, WorkoutListView, WorkoutDetailView, api_root
+from octofit_tracker.views import UserListView, UserDetailView, TeamListView, TeamDetailView, ActivityListView, ActivityDetailView, LeaderboardListView, LeaderboardDetailView, WorkoutListView, WorkoutDetailView, api_root
 
 urlpatterns = [
     path('', api_root, name='api-root'),
